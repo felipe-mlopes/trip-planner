aee43dc15709bcfc93bc0b03bf93c6d8986c5a9f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-14 18:17:22 -0300

    refactor: implement registerParticipantsToTrip method and add registerParticipantToTrip, triggerConfirmationEmailToParticipant and getAllParticipantsFromTrip methods

--- src/main/java/com/example/tripPlanner/participant/ParticipantService.java
+++ src/main/java/com/example/tripPlanner/participant/ParticipantService.java
@@ -1,5 +1,7 @@
 package com.example.tripPlanner.participant;
 
+import com.example.tripPlanner.trip.TripEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,7 +9,29 @@ import java.util.UUID;
 
 @Service
 public class ParticipantService {
-    public void registerParticipantsToTrip(List<String> participantsToInvite, UUID tripId){}
+
+    @Autowired
+    private ParticipantRepository repository;
+
+    public void registerParticipantsToTrip(List<String> participantsToInvite, TripEntity trip) {
+        List<ParticipantEntity> participants = participantsToInvite.stream().map(email -> new ParticipantEntity(email, trip)).toList();
+
+        this.repository.saveAll(participants);
+    }
+
+    public ParticipantCreateResponse registerParticipantToTrip(String email, TripEntity trip) {
+        ParticipantEntity newParticipant = new ParticipantEntity(email, trip);
+
+        this.repository.save(newParticipant);
+
+        return new ParticipantCreateResponse(newParticipant.getId());
+    }
 
     public void triggerConfirmationEmailToParticipants(UUID tripId) {}
+
+    public void triggerConfirmationEmailToParticipant(String email) {}
+
+    public List<ParticipantDataRecordDto> getAllParticipantsFromTrip(UUID tripId) {
+        return this.repository.findByTripId(tripId).stream().map(participant -> new ParticipantDataRecordDto(participant.getId(), participant.getName(), participant.getEmail(), participant.getIsConfirmed())).toList();
+    }
 }
