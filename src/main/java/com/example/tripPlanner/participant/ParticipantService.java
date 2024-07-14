b239af4ef02e03268d82d95bafaf3853eaf8a66f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-14 11:19:52 -0300

    feat: create participant service

---
+++ src/main/java/com/example/tripPlanner/participant/ParticipantService.java
@@ -0,0 +1,13 @@
+package com.example.tripPlanner.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+    public void registerParticipantsToTrip(List<String> participantsToInvite, UUID tripId){}
+
+    public void triggerConfirmationEmailToParticipants(UUID tripId) {}
+}
