d36d52cae43be2e195a8ce473e101e15e735304f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-18 20:24:07 -0300

    refactor: change ParticipantRepository to ParticipantService dependency, remove participant conditional and move logic to participant service

--- src/main/java/com/example/tripPlanner/controllers/ParticipantController.java
+++ src/main/java/com/example/tripPlanner/controllers/ParticipantController.java
@@ -3,6 +3,7 @@ package com.example.tripPlanner.controllers;
 import com.example.tripPlanner.entities.ParticipantEntity;
 import com.example.tripPlanner.controllers.dtos.ParticipantRecordDto;
 import com.example.tripPlanner.repositories.ParticipantRepository;
+import com.example.tripPlanner.services.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,23 +16,13 @@ import java.util.UUID;
 public class ParticipantController {
 
     @Autowired
-    private ParticipantRepository repository;
+    private ParticipantService participantService;
 
     @PostMapping("/{id}/confirm")
     public ResponseEntity<ParticipantEntity> confirmParticipant(@PathVariable UUID id, @RequestBody ParticipantRecordDto payload) {
-        Optional<ParticipantEntity> participant = this.repository.findById(id);
 
-        if(participant.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
+        ParticipantEntity confirmParticipant = this.participantService.confirmParticipant(id, payload.name());
 
-        ParticipantEntity rawParticipant = participant.get();
-
-        rawParticipant.setIsConfirmed(true);
-        rawParticipant.setName(payload.name());
-
-        this.repository.save(rawParticipant);
-
-        return ResponseEntity.ok(rawParticipant);
+        return ResponseEntity.ok(confirmParticipant);
     }
 }
