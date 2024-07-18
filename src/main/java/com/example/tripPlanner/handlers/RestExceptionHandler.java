b787e7502c266b345d2dec48135de6fe5b6d2a51
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-18 20:29:03 -0300

    refactor: change name to RestExceptionTripHandler, switch notFoundHandler to tripNotFoundHandler exception method and change TripNotFoundException to toTripFullErrorException and RestErrorMessage to RestErrorDto in fullErrorHandler exception method

--- src/main/java/com/example/tripPlanner/handlers/RestExceptionHandler.java
+++ src/main/java/com/example/tripPlanner/handlers/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.tripPlanner.handlers;
 
 import com.example.tripPlanner.controllers.dtos.RestErrorDto;
+import com.example.tripPlanner.exceptions.TripFullErrorException;
 import com.example.tripPlanner.exceptions.TripNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +11,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestExceptionTripHandler extends ResponseEntityExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(TripNotFoundException.class)
     @ResponseBody
-    private ResponseEntity<RestErrorDto> tripNotFoundHandler(TripNotFoundException exception) {
+    private ResponseEntity<RestErrorDto> notFoundHandler(TripNotFoundException exception) {
 
         RestErrorDto threatResponse = new RestErrorDto(HttpStatus.NOT_FOUND, exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(threatResponse);
     }
 
-//    @ExceptionHandler(TripNotFoundException.class)
-//    private ResponseEntity<RestErrorMessage> tripFullErrorHandler(TripNotFoundException exception) {
-//
-//        RestErrorMessage threatResponse = new RestErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(threatResponse);
-//    }
+    @ExceptionHandler(TripFullErrorException.class)
+    @ResponseBody
+    private ResponseEntity<RestErrorDto> fullErrorHandler(TripFullErrorException exception) {
+
+        RestErrorDto threatResponse = new RestErrorDto(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(threatResponse);
+    }
 }
