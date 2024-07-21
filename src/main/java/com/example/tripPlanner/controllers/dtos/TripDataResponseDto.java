6bb62623c24840d7ae93e671f899598bd53345a7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-20 22:01:30 -0300

    feat: create TripDataResponseDto dto

---
+++ src/main/java/com/example/tripPlanner/controllers/dtos/TripDataResponseDto.java
@@ -0,0 +1,7 @@
+package com.example.tripPlanner.controllers.dtos;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TripDataResponseDto(UUID id, String destination, LocalDateTime starts_at, LocalDateTime ends_at, Boolean is_confirmed, String owner_name, String owner_email) {
+}
