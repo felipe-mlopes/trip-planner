9d040313277e64d506d02a3ff214747463f961a4
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-22 15:53:57 -0300

    refactor: add not blank validations in title and occurs_at

--- src/main/java/com/example/tripPlanner/controllers/dtos/requests/ActivityRecordDto.java
+++ src/main/java/com/example/tripPlanner/controllers/dtos/requests/ActivityRecordDto.java
@@ -1,4 +1,12 @@
 package com.example.tripPlanner.controllers.dtos.requests;
 
-public record ActivityRecordDto(String title, String occurs_at) {
+import jakarta.validation.constraints.NotBlank;
+
+public record ActivityRecordDto(
+        @NotBlank(message = "O título não pode ser vazio")
+        String title,
+
+        @NotBlank(message = "A data da atividade não pode ser vazia")
+        String occurs_at
+) {
 }
