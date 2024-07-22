2f5c0f9fa03784c03065975962374c59c255eaf0
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-22 15:53:01 -0300

    feat: create record invalid date error exception

---
+++ src/main/java/com/example/tripPlanner/exceptions/RecordInvalidDateErrorException.java
@@ -0,0 +1,6 @@
+package com.example.tripPlanner.exceptions;
+
+public class RecordInvalidDateErrorException extends RuntimeException {
+
+    public RecordInvalidDateErrorException(String message) { super(message); }
+}
