f6f56946e3d58575666160602fdb7f93e26a68cd
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-22 15:54:45 -0300

    refactor: add not blank validation in name and not blank and email validation in email

--- src/main/java/com/example/tripPlanner/controllers/dtos/requests/ParticipantRecordDto.java
+++ src/main/java/com/example/tripPlanner/controllers/dtos/requests/ParticipantRecordDto.java
@@ -1,4 +1,14 @@
 package com.example.tripPlanner.controllers.dtos.requests;
 
-public record ParticipantRecordDto(String name, String email) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record ParticipantRecordDto(
+        @NotBlank(message = "O nome do participante não pode ser vazio")
+        String name,
+
+        @NotBlank(message = "O email do participante não pode ser vazio")
+        @Email(message = "Formato inválido de email")
+        String email
+) {
 }
