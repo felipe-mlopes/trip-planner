baecbca63b7d90f5d5e5ee5014e9bf98dd30550f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-22 15:55:22 -0300

    refactor: change future to not blank validation in starts_at and ends_at

--- src/main/java/com/example/tripPlanner/controllers/dtos/requests/TripRecordDto.java
+++ src/main/java/com/example/tripPlanner/controllers/dtos/requests/TripRecordDto.java
@@ -1,7 +1,6 @@
 package com.example.tripPlanner.controllers.dtos.requests;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
@@ -10,10 +9,10 @@ public record TripRecordDto(
         @NotBlank(message = "O destino não pode ser vazio")
         String destination,
 
-        @Future
+        @NotBlank(message = "A data de início não pode ser vazia")
         String starts_at,
 
-        @Future
+        @NotBlank(message = "A data de término não pode ser vazia")
         String ends_at,
 
         List<String> emails_to_invite,
